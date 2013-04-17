a23d0a467adbd651e7c3f4ae3b65cfa9b9b8a0c4
SaurabhPune <yash@SaurabhPunes-MacMini.local>
2013-04-17 16:42:22 +0530

    added 2 new files

---
+++ GitUseExa/MapOfUs.h
@@ -0,0 +1,13 @@
+//
+//  MapOfUs.h
+//  GitUseExa
+//
+//  Created by SaurabhPune on 17/04/13.
+//  Copyright (c) 2013 Infy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapOfUs : NSObject
+
+@end
---
+++ GitUseExa/MapOfUs.m
@@ -0,0 +1,13 @@
+//
+//  MapOfUs.m
+//  GitUseExa
+//
+//  Created by SaurabhPune on 17/04/13.
+//  Copyright (c) 2013 Infy. All rights reserved.
+//
+
+#import "MapOfUs.h"
+
+@implementation MapOfUs
+
+@end
