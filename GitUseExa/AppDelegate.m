217b35dcfe2fd02efbf1c4874472ea0794e4fac4
SaurabhPune <yash@SaurabhPunes-MacMini.local>
2013-04-17 14:46:25 +0530

    added some whitespaces in app delegate ...test commit

--- GitUseExa/AppDelegate.m
+++ GitUseExa/AppDelegate.m
@@ -15,6 +15,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    
+    
+    
     // Override point for customization after application launch.
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
